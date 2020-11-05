81e2d4d2d31269278f324039eb659281cec67bd7
justlittledream <1074726545@qq.com>
2020-11-05 15:00:01 +0800

    请求数多了后，会出现端口占用

--- cmd/push.go
+++ cmd/push.go
@@ -17,11 +17,9 @@ package cmd
 
 import (
 	"bytes"
-	"context"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"sort"
 	"strconv"
 	"sync"
 	"time"
@@ -29,34 +27,27 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// type label struct{
-// 	key string
-// 	value string
-// }
-
-// type promtailStream struct {
-// 	Labels string       `json:"stream"`
-// 	Values [1][2]string `json:"values"`
-// }
-
-// type promtailMsg struct {
-// 	Streams []promtailStream `json:"streams"`
-// }
-
 //var finishedRequest = make(chan time.Duration, 100000)
 //var badRequest = make(chan time.Duration, 100000)
+// var finishedRequest = make(map[int]time.Duration)
+// var badRequest = make(map[int]time.Duration)
+var finishedRequest = make([]int, 60001)
+var badRequest = make([]int, 60001)
 
-var finishedRequest = make(map[int]time.Duration)
-var badRequest = make(map[int]time.Duration)
+// var pushChan = make(chan int, 1)
+//var count = 0
 var lock sync.Mutex
 
 // pushCmd represents the push command
 var pushCmd = &cobra.Command{
 	Use:   "push",
 	Short: "push logs to loki",
-	Long:  `----------help----------------`,
+	Long: `----------help----------------
+	example 
+	`,
 	Run: func(cmd *cobra.Command, args []string) {
 		startTime := time.Now()
+		url, _ := cmd.Flags().GetString("url")
 		key, _ := cmd.Flags().GetString("key")
 		value, _ := cmd.Flags().GetString("value")
 		task, _ := cmd.Flags().GetInt("task")
@@ -64,14 +55,16 @@ var pushCmd = &cobra.Command{
 		max, _ := cmd.Flags().GetInt("max")
 		fmt.Printf("\nPush %v logs to loki with max concurrent tasks = %v\n", task, max)
 		//fmt.Printf("\nbegin to send logs\n")
-		costTime := sendLogs(key, value, task, max)
+		costTime := sendLogs(url, key, value, task, max)
 		//fmt.Printf("\nresults is as followed\n")
-		calculate(startTime, costTime)
+		calculate(costTime)
+		fmt.Printf("\ntotal time for test:\t%v\n\n", time.Now().Sub(startTime))
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(pushCmd)
+	pushCmd.PersistentFlags().StringP("url", "u", "http://localhost:3100/loki/api/v1/push", "url of loki http api")
 	pushCmd.PersistentFlags().StringP("key", "k", "app", "key of label")
 	pushCmd.PersistentFlags().StringP("value", "v", "server", "value of label")
 	pushCmd.PersistentFlags().IntP("task", "t", 1000, "number of logs")
@@ -98,21 +91,20 @@ input：
 output：
 	endtime
 */
-func sendLogs(key, value string, task, max int) time.Duration {
+func sendLogs(url, key, value string, task, max int) time.Duration {
 	//统一了推送日志流的时间
 	postTime := time.Now().UnixNano() // 纳秒
 	logTime := strconv.FormatInt(int64(postTime), 10)
 	//记录执行发送第一条请求前的时间
 	startTime := time.Now()
-
+	//fmt.Printf("\n %v  %v", task, max)
 	channel := make(chan int, max) //控制协程最大数
 	for i := 0; i < task; i++ {
-		channel <- 1
-		go send(key, value, i, channel, logTime)
+		go send(url, key, value, i, channel, logTime)
 	}
 	for {
 		if len(channel) == 0 {
-			return time.Now().Sub(startTime)
+			return time.Now().Sub(startTime) //返回完成全部请求所用的时间
 		}
 	}
 }
@@ -124,105 +116,146 @@ func sendLogs(key, value string, task, max int) time.Duration {
 	channel: chan of task
 	t: current time
 */
-func send(key string, value string, i int, channel chan int, logTime string) {
-	// time := time.Now().UnixNano() // 纳秒
-	// t := strconv.FormatInt(int64(time), 10)
-	url := "http://localhost:31000/loki/api/v1/push"
+func send(url string, key string, value string, i int, channel chan int, logTime string) {
+	channel <- 1
+	defer func() { <-channel }()
+	//url := "http://localhost:31000/loki/api/v1/push"
 	logs := "line " + strconv.FormatInt(int64(i), 10) + " : test for log"
 	//请求内容符合loki http api要求
 	test := "{\"streams\":[{\"stream\":{\"" + key + "\":\"" + value + "\"},\"values\":[[\"" + logTime + "\",\"" + logs + "\"]]}]}"
 	request, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(test)))
 	if err != nil {
 		fmt.Printf(" request err: http.NewRequest%v", err)
-		//return queryobj, err
+
 	}
 	request.Header.Set("Content-Type", "application/json")
 	client := http.Client{}
 	startTime := time.Now()
-	resp, err := client.Do(request.WithContext(context.TODO())) //发送请求
+	resp, err := client.Do(request) //发送请求
 	costTime := time.Now().Sub(startTime)
-	//fmt.Println(costTime)
-	// 如果请求成功，将耗费时间传入finishedRequest通道，否则传入badRequest通道
-	defer resp.Body.Close()
+	index := int(costTime/time.Millisecond) + 1
+	//fmt.Printf("costTime: %v   Index: %v\n", costTime, index)
 	if err != nil {
 		fmt.Printf("client.Do%v", err)
-		badRequest[i] = costTime
+		addTime(badRequest, index)
 		//badRequest <- costTime
 	} else {
 		//finishedRequest <- costTime
-		finishedRequest[i] = costTime
+		addTime(finishedRequest, index)
 	}
+	resp.Body.Close()
 	respBytes, err := ioutil.ReadAll(resp.Body)
+
 	if err != nil {
 		fmt.Printf("ioutil.ReadAll%v", err)
 	}
-	fmt.Print(string(respBytes))
-	<-channel //表示该协程已经结束
-	// res, err := http.Post(url, header, bytes.NewBuffer([]byte(test)))
-	// if err != nil {
-	// 	fmt.Println("Fatal error ", err.Error())
-	// }
-	// content, err := ioutil.ReadAll(res.Body)
-	// if err != nil {
-	// 	fmt.Println("Fatal error ", err.Error())
-	// }
-	// fmt.Println(string(content))
-	// str := (*string)(unsafe.Pointer(&content)) //转化为string,优化内存
-	// fmt.Println(*str)
-
+	if string(respBytes) != "" {
+		fmt.Println(string(respBytes))
+	}
 }
 
-func calculate(startTime time.Time, costTime time.Duration) {
-	var times = []time.Duration{}
-	fmt.Printf("\nFinished requests:\t%v\nFailed requests:\t%v\n\n", len(finishedRequest), len(badRequest))
-	//fmt.Println(len(finishedRequest))
-	//close(finishedRequest)
-	for i := range finishedRequest {
-		times = append(times, finishedRequest[i])
+func calculate(costTime time.Duration) {
+	sumTime := 0
+	count := 0 //总共的request数
+	for i, v := range finishedRequest {
+		if v != 0 {
+			//fmt.Printf("index: %v   count: %v\n", i, v)
+			sumTime += i * v
+			count += v
+		}
+
 	}
-	//fmt.Println(len(times))
-	sort.Slice(times, func(i, j int) bool {
-		return times[i] < times[j]
-	})
-	var sumTime time.Duration
-	for i := range times {
-		//fmt.Println(times[i])
-		sumTime += times[i]
+	badCount := 0
+	for _, v := range badRequest {
+		//sumTime += i * v
+		badCount += v
 	}
-	//fmt.Printf("\ncostTime for all requests: %v\n", costTime)
-	count := time.Duration(len(times)) * time.Nanosecond
-	meanTimes := sumTime / count
-	meanTimesCurrentTask := costTime / count
-	fmt.Printf("Time per request:\t%v (mean)\n", meanTimes)
-	fmt.Printf("Time per request:\t%v (mean,across all concurrent requests)\n\n", meanTimesCurrentTask)
+	fmt.Printf("\nFinished requests:\t%v\nFailed requests:\t%v\n\n", count, badCount)
+
+	meanTimes := sumTime/count + 1
+	//fmt.Println(costTime)
+	meanTimesCurrentTask := (float64(costTime/time.Millisecond) + 1) / float64(count)
+	fmt.Printf("Time per request:\t%vms (mean)\n", meanTimes)
+	fmt.Printf("Time per request:\t%vms (mean,across all concurrent requests)\n\n", meanTimesCurrentTask)
 	//fmt.Printf("Requests per second:\t%v", int64(time.Second / times[1]))
 	//fmt.Print(times[0])
-	fmt.Println("Percentage of the requests served within a certain time:")
-	for i := 50; i < 100; i = i + 10 {
-		j := i * len(times) / 100
-		fmt.Printf("%v%%\t%v\n", i, times[j-1])
+	var index = 0
+	// var percentage
+	var output = make(map[int]int)
+	for i, v := range finishedRequest {
+		index += v
+		//fmt.Println(i, v)
+		if v != 0 {
+			var per = index * 100 / count
+			//fmt.Println(i, v, per)
+			switch {
+			case per == 100:
+				if _, ok := output[100]; !ok {
+					output[100] = i
+				}
+				fallthrough
+				//fmt.Printf("%v%%\t%v(longest request)\n", 100, i)
+			case per >= 99:
+				if _, ok := output[99]; !ok {
+					output[99] = i
+				}
+				fallthrough
+				//fmt.Printf("%v%%\t%v\n", 99, i)
+			case per >= 95:
+				if _, ok := output[95]; !ok {
+					output[95] = i
+				}
+				fallthrough
+				//fmt.Printf("%v%%\t%v\n", 95, i)
+			case per >= 90:
+				if _, ok := output[90]; !ok {
+					output[90] = i
+				}
+				fallthrough
+				//fmt.Printf("%v%%\t%v\n", 90, i)
+			case per >= 80:
+				if _, ok := output[80]; !ok {
+					output[80] = i
+				}
+				fallthrough
+				//fmt.Printf("%v%%\t%v\n", 80, i)
+			case per >= 70:
+				if _, ok := output[70]; !ok {
+					output[70] = i
+				}
+				fallthrough
+				//fmt.Printf("%v%%\t%v\n", 70, i)
+			case per >= 60:
+				if _, ok := output[60]; !ok {
+					output[60] = i
+				}
+				fallthrough
+				//fmt.Printf("%v%%\t%v\n", 60, i)
+			case per >= 50:
+				if _, ok := output[50]; !ok {
+					output[50] = i
+				}
+				//fmt.Printf("%v%%\t%v\n", 50, i)
+			}
+		}
 	}
-	fmt.Printf("%v%%\t%v\n", 95, times[95*len(times)/100-1])
-	fmt.Printf("%v%%\t%v\n", 99, times[99*len(times)/100-1])
-	fmt.Printf("%v%%\t%v(longest request)\n", 100, times[len(times)-1])
-
-	fmt.Printf("\ntotal time for test:\t%v\n\n", time.Now().Sub(startTime))
+	printOut(output)
 }
 
-func addTime(request map[int]time.Duration, t time.Duration) {
-
+func addTime(request []int, i int) {
+	lock.Lock()
+	request[i]++
+	lock.Unlock()
 }
 
-// func pushLogs(key string, value string, fre int, i int) {
-// 	for {
-// 		//fmt.Printf("push %v\n", i)
-// 		//send(key, value, i)
-// 		fmt.Printf("client %v send a log-------\n", i)
-// 		time.Sleep(time.Second * time.Duration(fre))
-// 	}
-// 	// wg.Done()
-// }
-
-// key: key of label
-// value: value of label
-// i： client i
+func printOut(output map[int]int) {
+	fmt.Println("Percentage of the requests served within a certain time(ms):")
+	fmt.Printf("%v%%\t%v\n", 50, output[50])
+	fmt.Printf("%v%%\t%v\n", 60, output[60])
+	fmt.Printf("%v%%\t%v\n", 70, output[70])
+	fmt.Printf("%v%%\t%v\n", 80, output[80])
+	fmt.Printf("%v%%\t%v\n", 90, output[90])
+	fmt.Printf("%v%%\t%v\n", 95, output[95])
+	fmt.Printf("%v%%\t%v\n", 99, output[99])
+	fmt.Printf("%v%%\t%v\n", 100, output[100])
+}
